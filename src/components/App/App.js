28c4d2c31f94e50c9b8c04b8507eb88ff96b3fd9
vrastello <vincent.rastello@spin.pm>
2023-12-14 00:13:36 -0800

    adding login page

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,17 @@
-import React from 'react';
+import React , { useState } from 'react';
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Dashboard from '../Dashboard/Dashboard';
+import Login from '../Login/Login';
 import Offers from '../Offers/Offers';
 
 function App() {
+  const [token, setToken] = useState();
+
+  if(!token) {
+    return <Login setToken={setToken} />
+  }
+  
   return (
     <div className="wrapper">
       <h1>Influence Mobile</h1>
