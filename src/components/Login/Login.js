3f88aceb267c512dacf196491722e5f908b9af2d
vrastello <vincent.rastello@spin.pm>
2023-12-14 00:45:22 -0800

    adding simple async login function and getting rid of warning from babel

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,17 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
 
-export default function Login() {
+import './Login.css';
+
+async function loginUser(credentials) {
+ return fetch('http://localhost:3002/auth/login', {
+   method: 'POST',
+   headers: {
+     'Content-Type': 'application/json'
+   },
+   body: JSON.stringify(credentials)
+ })
+   .then(data => data.json())
+}
+
+export default function Login( { setToken } ) {
+  const [username, setUserName] = useState();
+  const [password, setPassword] = useState();
   return (
     <div className="login-wrapper">
       <h1>Please Log In</h1>
       <form>
         <label>
           <p>Username</p>
-          <input type="text" />
+          <input type="text" onChange={e => setUserName(e.target.value)}/>
         </label>
         <label>
           <p>Password</p>
-          <input type="password" />
+          <input type="password" onChange={e => setPassword(e.target.value)}/>
         </label>
         <div>
           <button type="submit">Submit</button>
@@ -19,4 +35,8 @@ export default function Login() {
       </form>
     </div>
   )
+}
+
+Login.propTypes = {
+  setToken: PropTypes.func.isRequired
 }
