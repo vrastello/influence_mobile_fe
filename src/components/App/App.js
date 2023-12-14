1b32dbca1c78c1eff2d845de69b2097ab46343cf
vrastello <vincent.rastello@spin.pm>
2023-12-14 00:04:58 -0800

    updating for react 18

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,22 @@
+import React from 'react';
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Dashboard from '../Dashboard/Dashboard';
+import Offers from '../Offers/Offers';
 
 function App() {
-  return <></>
+  return (
+    <div className="wrapper">
+      <h1>Influence Mobile</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/offers" element={<Offers />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+
+  )
 }
 
 export default App;
